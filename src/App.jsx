79cf7967c13e34b23fe3c9839c9ce1920a22270b
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-02 09:42:34 -0300

    get random quote functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [quote, setQuote] = useState({ text: '', author: '' })
+
+  useEffect(() => {
+    fetch('https://api.quotable.io/random')
+      .then((res) => res.json())
+      .then((data) => setQuote({ text: data.content, author: data.author }))
+  }, [])
+
   return (
     <div>
-      <h1>Hello World 👋</h1>
+      <h1>Quote of the Day</h1>
+      <p id="text">{quote.text}</p>
+      <p id="author">{quote.author}</p>
     </div>
   )
 }
